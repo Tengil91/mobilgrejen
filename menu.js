7e4f973866096041c636b86625da4229de8ebbd4
eriksson <sebbe.eriksson09@icloud.com>
2019-08-22 15:51:02 +0200

    menuKlar

--- menu.js
+++ menu.js
@@ -1,47 +1,65 @@
 menuClicked = false;
 
 document.querySelector("#menuButton").addEventListener("click", function() {
+  let hideMe = document.querySelectorAll(".hideMe");
   if (menuClicked === false) {
     list = document.querySelectorAll(".menuHide");
-    draperi = document.querySelector(".draperi");
-    draperi.classList.add("draperiCover");
-    yo();
-    /* list.forEach(li => {
-      li.classList.add("menuShow");
-      li.classList.remove("menuHide");
-    }); */
 
+    hideMe.forEach(element => {
+      element.classList.add("hideElement");
+      element.classList.remove("showElement");
+    });
+
+    showMenu();
     menuClicked = true;
   } else {
-    /* let listhide = document.querySelectorAll(".menuShow"); */
-
-    /* listhide.forEach(li => {
-      li.classList.add("menuHide");
-      li.classList.remove("menuShow");
-    }); */
-
-    draperi.classList.remove("draperiCover");
     menuClicked = false;
+    hideMenu();
+    hideMe.forEach(element => {
+      element.classList.remove("hideElement");
+      element.classList.add("showElement");
+    });
+    //document.querySelector(".").classList.remove("hideElement");
   }
 });
 
-function yo() {
+function showMenu() {
   anime({
     targets: ".menuHide",
-    delay: function(el, i) {
-      i * 40;
-    },
+
     translateX: {
-      value: 250,
-      duration: 800
+      value: 1000,
+      duration: 4000
+    },
+    scale: 4,
+
+    translateY: {
+      value: function(el, i) {
+        return i * 40;
+      }
+    },
+    delay: function(el, i) {
+      return i * 100;
+    }
+  });
+}
+
+function hideMenu() {
+  anime({
+    targets: ".menuHide",
+
+    delay: function(el, i) {
+      return i * 100;
     },
 
+    translateX: {
+      value: -250,
+      duration: 4000
+    },
     translateY: {
       value: function(el, i) {
         return i * 80;
       }
-    },
-
-    delay: 250 // All properties except 'scale' inherit 250ms delay
+    }
   });
 }
